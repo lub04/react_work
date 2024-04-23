8a2c61e2ec5583d1aa08c63ed7952adbc59cb946
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-23 13:06:11 +0200

    modif react et css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const hello = () =>{
+    return(
+    <h1 style={{color: "green"}}>Hello react World</h1>
+  )
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 style={{color: "green"}}>Hello react World</h1>
+      <header>
+        <img src="https://img.freepik.com/vecteurs-libre/lettre-coloree-creation-logo-degrade_474888-2309.jpg?size=338&ext=jpg&ga=GA1.1.2116175301.1713657600&semt=ais" alt="" />
+        <nav className='nav'>
+          <a href="">accueil</a>
+          <a href="">filtres</a>
+          <a href="">contacts</a>
+        </nav>
+      </header>
+      {hello()}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
